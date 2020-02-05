4cd6d24d7b9a03f19fd16e5be3eeb27fa612f961
andrew.kwon <andrew.kwon@mu-c1-03.mullen.pvt.k12.co.us>
2020-02-05 13:56:17 -0700

    Test for the graphics aspect for the game (just trying stuff out)
    https://github.com/aelmosalamy/ascii-combat (link for anscii combat)

--- midTerm/src/NewMainGameTest/Utils.java
+++ midTerm/src/NewMainGameTest/Utils.java
@@ -1,6 +1,66 @@
 package NewMainGameTest;
 
-public class Utils
-	{
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
+public class Utils {
+	
+	//File stored within Jar, Immutable
+	public static String loadWorld(String path){
+		StringBuilder builder = new StringBuilder();
+		
+		try{
+			InputStream in = InputStream.class.getResourceAsStream(path); 
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			//BufferedReader br = new BufferedReader(new FileReader(path));
+			String line;
+			while((line =  br.readLine()) != null)
+				builder.append(line + "\n");
+			
+			br.close();
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+		
+		return builder.toString();
 	}
+	
+	//File stored outside Jar, mutable
+	public static String loadFile(String path, boolean newgame){
+		StringBuilder builder = new StringBuilder();
+		
+		try{
+			//InputStream in = InputStream.class.getResourceAsStream(path); 
+			//BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			BufferedReader br;
+			if (newgame)
+				br = new BufferedReader(new FileReader("saveState-NewGame.txt"));
+			else
+				br = new BufferedReader(new FileReader("saveState.txt"));
+			String line;
+			while((line =  br.readLine()) != null)
+				builder.append(line + "\n");
+			
+			br.close();
+		}catch(IOException e){
+			//loadWorld("/saveFiles/" + path);
+			e.printStackTrace();
+		}
+		
+		return builder.toString();
+	}
+	
+	public static int parseInt(String number){
+		try{
+			return Integer.parseInt(number);
+		}catch(NumberFormatException e){
+			e.printStackTrace();
+			return 0;
+		}
+	}
+	
+}
+
--- midTerm/src/midTerm/NewMainGameTest.java
+++ midTerm/src/midTerm/NewMainGameTest.java
@@ -1,31 +1,7 @@
 package midTerm;
 
-public class NewMainGameTest
-	{
-		 public static void main(String [] args)
-		 {
-			 player();
-			 Game ex = new Game();               //Creates new object of type Game, and name it ex.
-	         new Thread(ex).start();  	 		//Start this so called ex object. 
-		 }
-		 public static void player()
-		 {
-			 //variables which we will use 
-			 int x, y;
-			 boolean  left, right, up, down;
-		 }
-		 private static void update()
-			{
-
-			}
-	}
-public class Game implements Runnable
+public class NewMainGameTest implements Runnable
 {
-	public void run()
-	{
-		 while(true)
-		System.out.println(" While ! ");
-	}
-	   
+	
 }
 
